755137f1012c42dd2bf7d995b13bd9b4163245de
buo-yant <106121929+buo-yant@users.noreply.github.com>
2022-07-05 01:03:02 +0100

    Create Function.py
    Creating a function to make my code easier by calling onto the function to perform a particular task

---
+++ Function.py
@@ -0,0 +1,14 @@
+def greetuser():
+  print("Hi, BUOYANT")
+
+greetuser()    #calling the function
+Output 
+>>>Hi BUOYANT
+
+def addNumber(num1,num1):
+  sum = num1 + num2
+  Print (sum)
+
+addNumber(2,3)   #calling the function to perform a specific task
+Output 
+>>>5
